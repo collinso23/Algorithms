9e16df0091dfeadf1190b045c4602ea7bc2b4096
Orion Collins <collinso@wit.edu>
2019-06-10 11:35:47 -0400

    removed incompletes

--- comp2350-A6/src/edu/wit/cs/comp2350/Speller.java
+++
@@ -1,9 +0,0 @@
-package edu.wit.cs.comp2350;
-
-public abstract class Speller {
-
-	public abstract void insertWord(String s);
-	public abstract boolean contains(String s);
-	public abstract String[] getSuggestions(String s);
-
-}
